bae49637b47456c32a5b77547a6bbd5b09558bd2
Anton <antony.adamson@gmail.com>
2023-05-11 22:11:16 +0600

    Start point

--- MusicWireframe/ViewController.swift
+++ MusicWireframe/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var albumImageView: UIImageView!
+    @IBOutlet var reverseBackground: UIView!
+    @IBOutlet var playPauseBackground: UIView!
+    @IBOutlet var forwardBackground: UIView!
+    @IBOutlet var reverseButton: UIButton!
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var forwardButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
