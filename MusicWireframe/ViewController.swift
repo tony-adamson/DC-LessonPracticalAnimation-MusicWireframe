a1ec9822a6963af39462f48829e169d483d5ea36
Anton <antony.adamson@gmail.com>
2023-05-11 21:14:11 +0600

    Initial Commit

---
+++ MusicWireframe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicWireframe
+//
+//  Created by Антон Адамсон on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
