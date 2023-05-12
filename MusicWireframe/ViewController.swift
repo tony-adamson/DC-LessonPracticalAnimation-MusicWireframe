fbfd4732f87ee26a7045890766d14e103fb9c3b9
Anton <antony.adamson@gmail.com>
2023-05-12 14:44:34 +0600

    album art animation

--- MusicWireframe/ViewController.swift
+++ MusicWireframe/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Отслеживание состояния кнопки проигрывания песни и наблюдатель для обновления состояния
+    var isPlaying: Bool = true {
+        didSet {
+            playPauseButton.isSelected = isPlaying
+        }
+    }
+    
+    //MARK: - Outlets
     @IBOutlet var albumImageView: UIImageView!
     @IBOutlet var reverseBackground: UIView!
     @IBOutlet var playPauseBackground: UIView!
@@ -16,11 +24,46 @@ class ViewController: UIViewController {
     @IBOutlet var reverseButton: UIButton!
     @IBOutlet var playPauseButton: UIButton!
     @IBOutlet var forwardButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Скругляем углы и должен ли видимый слой представления ограничиваться границами представления
+        /*
+        reverseBackground.layer.cornerRadius = reverseBackground.frame.height / 2
+        reverseBackground.clipsToBounds = true
+        
+        playPauseBackground.layer.cornerRadius = playPauseBackground.frame.height / 2
+        playPauseBackground.clipsToBounds = true
+        
+        forwardBackground.layer.cornerRadius = forwardBackground.frame.height / 2
+        forwardBackground.clipsToBounds = true
+        */
+        
+        [reverseBackground, playPauseBackground, forwardBackground].forEach { view in
+            view?.layer.cornerRadius = view!.frame.height / 2
+            view?.clipsToBounds = true
+            view?.alpha = 0.0
+        }
     }
-
+    
+    // Действие при нажатии на кнопку
+    @IBAction func playPauseButtonTapped() {
+        // Меняем тру на фолс и наоборот
+        isPlaying.toggle()
+        
+        // Далее действия анимации исходя из значения
+        if isPlaying {
+            UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: [], animations: {
+                self.albumImageView.transform = CGAffineTransform.identity
+            }, completion: nil)
+        } else {
+            UIView.animate(withDuration: 0.5) {
+                self.albumImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+            }
+        }
+    }
+    
 
 }
 
