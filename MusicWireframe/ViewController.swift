7ab1be3ef994c66bdcb8f4cc8c0db5fb08440e99
Anton <antony.adamson@gmail.com>
2023-05-14 14:37:21 +0600

    Final

--- MusicWireframe/ViewController.swift
+++ MusicWireframe/ViewController.swift
@@ -64,6 +64,34 @@ class ViewController: UIViewController {
         }
     }
     
-
+    @IBAction func touchedUpInside(_ sender: Any) {
+    }
+    
+    // Действия после касания пальцем кнопки
+    @IBAction func touchedDown(_ sender: UIButton) {
+        let buttonBackground: UIView
+        
+        // Включения фона кнопки
+        switch sender {
+        case reverseButton:
+            buttonBackground = reverseBackground
+        case playPauseButton:
+            buttonBackground = playPauseBackground
+        case forwardButton:
+            buttonBackground = forwardBackground
+        default:
+            return
+        }
+        
+        // Анимация фона
+        UIView.animate(withDuration: 0.5, animations: {
+                buttonBackground.alpha = 0.0
+                buttonBackground.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                sender.transform = CGAffineTransform.identity
+            }) { (_) in
+                buttonBackground.transform = CGAffineTransform.identity
+            }
+    }
+    
 }
 
